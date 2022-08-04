c334ebec14aaaad29add95b1b9391220b8995ec6
Yevheniia Suska <susskaya86@gmail.com>
2022-08-04 04:36:31 +0300

    install redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import { configureStore } from '@reduxjs/toolkit';
+import { App } from 'App';
 import './index.css';
+
+const reducer = (state = {}, action) => state;
+// const store = configureStore(reducer);
+const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
